def1e476d6bbee241b23ce5f13887d0c6fbc1ecc
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-03-24 17:33:34 +0300

    W/o &

--- src/managers/ai/aifunctions.cpp
+++ src/managers/ai/aifunctions.cpp
@@ -28,11 +28,11 @@ namespace {
 
 		if (weapon_L) {
 			log::info("Dropping weapon L");
-			tiny->DropObject(weapon_L, nullptr, 1.0, &coords, &coords);
+			tiny->DropObject(weapon_L, nullptr, 1.0, coords, coords);
 		}
 		if (weapon_R) {
 			log::info("Dropping weapon R");
-			tiny->DropObject(weapon_R, nullptr, 1.0, &coords, &coords);
+			tiny->DropObject(weapon_R, nullptr, 1.0, coords, coords);
 		}
 	}
 
