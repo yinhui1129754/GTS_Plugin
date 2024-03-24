db83623e6bd895f5aa82c148931df38222021ff3
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-03-24 11:26:48 +0300

    Actor::

--- src/managers/GtsManager.cpp
+++ src/managers/GtsManager.cpp
@@ -80,7 +80,7 @@ namespace {
 			auto node = find_node(actor, "skeleton_female.nif");
 			NiAVObject* skeleton = node;
 
-			actor->GetActorRuntimeData().boolBits.set(BOOL_BITS::kUseEmotion);
+			actor->GetActorRuntimeData().boolBits.set(Actor::BOOL_BITS::kUseEmotion);
 			/*auto ref = actor->GetCharController();
 			if (ref) {
 				log::info("LOD Distance of {} is {}", actor->GetDisplayFullName(), ref->lodDistance);
