c4d878f7a8d9b2dab281cccdfd461e95b46f20ae
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-03-24 17:19:40 +0300

    Empty

--- src/managers/ai/aifunctions.cpp
+++ src/managers/ai/aifunctions.cpp
@@ -23,7 +23,7 @@ namespace {
 		TESBoundObject* weapon_L = tiny->GetEquippedObject(true)->As<RE::TESBoundObject>();
 		TESBoundObject* weapon_R = tiny->GetEquippedObject(false)->As<RE::TESBoundObject>();
 
-		NiPoint3& coords = NiPoint3(0.0, 0.0, 0.0);
+		NiPoint3& coords = NiPoint3();
 
 		if (weapon_L) {
 			log::info("Dropping weapon L");
