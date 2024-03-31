f0d8691ad3253e515c0faec983bda0e81b052f89
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-03-31 16:34:50 +0300

    Try to reduce checks in PushActorAway

--- src/utils/actorUtils.cpp
+++ src/utils/actorUtils.cpp
@@ -1495,15 +1495,13 @@ namespace Gts {
 		if (source) {
 			auto ai = source->GetActorRuntimeData().currentProcess;
 			if (ai) {
-				if (ai->InHighProcess()) {
-					if (receiver->Is3DLoaded()) {
-						if (source->Is3DLoaded()) {
-							NiPoint3 direction = receiver->GetPosition() - source->GetPosition();
-							direction = direction / direction.Length();
-							typedef void (*DefPushActorAway)(AIProcess *ai, Actor* actor, NiPoint3& direction, float force);
-							REL::Relocation<DefPushActorAway> RealPushActorAway{ RELOCATION_ID(38858, 39895) };
-							RealPushActorAway(ai, receiver, direction, afKnockBackForce);
-						}
+				if (receiver->Is3DLoaded()) {
+					if (source->Is3DLoaded()) {
+						NiPoint3 direction = NiPoint3();//receiver->GetPosition() - source->GetPosition();
+						//direction = direction / direction.Length();
+						typedef void (*DefPushActorAway)(AIProcess *ai, Actor* actor, NiPoint3& direction, float force);
+						REL::Relocation<DefPushActorAway> RealPushActorAway{ RELOCATION_ID(38858, 39895) };
+						RealPushActorAway(ai, receiver, direction, afKnockBackForce);
 					}
 				}
 			}
