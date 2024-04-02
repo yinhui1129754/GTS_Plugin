ed5f2fe8910f65069629e7be36c0f636d1c905ef
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-04-02 21:07:58 +0300

    &'s

--- src/utils/MovementForce.cpp
+++ src/utils/MovementForce.cpp
@@ -17,7 +17,7 @@ namespace Gts {
 		float NodeMovementForce = 0.0;
 		float scale = get_visual_scale(giant);
 		
-		NiPoint3 DataCoordinates = NiPoint3();
+		NiPoint3& DataCoordinates = NiPoint3();
 		NiPoint3 InputCoordinates = NiPoint3();
 		
 		auto Data = Transient::GetSingleton().GetData(giant);
@@ -30,38 +30,42 @@ namespace Gts {
 			switch (Type) {
 				case NodeMovementType::Movement_LeftLeg: 
 					Node = find_node(giant, "NPC L Foot [Lft ]");
-					DataCoordinates = Data->POS_Last_Leg_L;
+					NiPoint3& LL = Data->POS_Last_Leg_L;
+					DataCoordinates = LL;
 				break;
 				case NodeMovementType::Movement_RightLeg: 
 					Node = find_node(giant, "NPC R Foot [Rft ]");
-					DataCoordinates = Data->POS_Last_Leg_R;
+					NiPoint3& RL = Data->POS_Last_Leg_R;
+					DataCoordinates = RL;
 				break;
 				case NodeMovementType::Movement_LeftHand: 
 					Node = find_node(giant, "NPC L Hand [LHnd]");
-					DataCoordinates = Data->POS_Last_Hand_L;
+					NiPoint3& LH = Data->POS_Last_Hand_L;
+					DataCoordinates = LH;
 				break;
 				case NodeMovementType::Movement_RightHand: 
 					Node = find_node(giant, "NPC R Hand [RHnd]");
-					DataCoordinates = Data->POS_Last_Hand_R;
+					NiPoint3& RH = Data->POS_Last_Hand_R;
+					DataCoordinates = RH;
 				break;
 			}
 
 			if (Node) {
 				InputCoordinates = Node->world.translate; // Record input node coordinates
 				log::info("Input coords: {}", Vector2Str(InputCoordinates));
-				if (InputCoordinates.Length() > 0 && DataCoordinates.Length() > 0) {
-					NodeMovementForce = (InputCoordinates - DataCoordinates).Length();
 
-					// ^ Compare values, get movement force of Node X over 1 frame
-					// ^ And also compensate speed with scale, since nodes travel further distance at large distances
-				}
-				if (TimePassed_This > TimePassed_Data) {
+				NodeMovementForce = (InputCoordinates - DataCoordinates).Length();
+				// ^ Compare values, get movement force of Node X over 1 frame
+				// ^ And also compensate speed with scale, since nodes travel further distance at large distances
+				
+				log::info("Data coords: {}", Vector2Str(DataCoordinates));
+				if (TimePassed_This != TimePassed_Data) {
 					TimePassed_Data = TimePassed_This;
 					DataCoordinates = InputCoordinates; // Record new pos of bone
 					log::info("Recording new coords");
 				}
-				log::info("Data coords: {}", Vector2Str(DataCoordinates));
-				log::info("TimePassed: Data: {}, (Transient: {}), This: {}", TimePassed_Data, Data->POS_LastCheckTime, TimePassed_This);
+				
+				//log::info("TimePassed: Data: {}, (Transient: {}), This: {}", TimePassed_Data, Data->POS_LastCheckTime, TimePassed_This);
 			}
 		}
 		
