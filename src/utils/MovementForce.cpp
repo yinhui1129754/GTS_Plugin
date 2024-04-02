576923eb94595e99b8e81518acbf01b6beccc99e
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-04-02 18:23:22 +0300

    Fixes

--- src/utils/MovementForce.cpp
+++ src/utils/MovementForce.cpp
@@ -3,6 +3,7 @@
 #include "UI/DebugAPI.hpp"
 #include "scale/scale.hpp"
 #include "rays/raycast.hpp"
+#include "node.hpp"
 
 using namespace Gts;
 using namespace RE;
@@ -13,47 +14,46 @@ namespace Gts {
 	float Get_Bone_Movement_Speed(Actor* giant, NodeMovementType Type) {
 		float scale = get_visual_scale(giant);
 		auto& Data = MovementForce::GetData(giant);
-		if (Data) {
 
-			NiPoint3 InputCoordinates = NiPoint3();
-			NiPoint3 DataCoordinates = NiPoint3();
-			NiAVObject* Node = nullptr;
+		NiPoint3 InputCoordinates = NiPoint3();
+		NiPoint3 DataCoordinates = NiPoint3();
+		NiAVObject* Node = nullptr;
 
-			float MovementForce = 0.0;
-			
-			switch (Type) {
-				case NodeMovementType::Movement_LeftLeg: 
-					Node = find_node(giant, "NPC L Foot [Lft ]");
-					DataCoordinates = Data.POS_Last_Leg_L;
-				break;
-				case NodeMovementType::Movement_RightLeg: 
-					Node = find_node(giant, "NPC R Foot [Rft ]");
-					DataCoordinates = Data.POS_Last_Leg_R;
-				break;
-				case NodeMovementType::Movement_LeftHand: 
-					Node = find_node(giant, "NPC L Hand [LHnd]");
-					DataCoordinates = Data.POS_Last_Hand_L;
-				break;
-				case NodeMovementType::Movement_RightHand: 
-					Node = find_node(giant, "NPC R Hand [RHnd]");
-					DataCoordinates = Data.POS_Last_Hand_R;
-				break;
-			}
-
-			if (Node) {
-				InputCoordinates = Node->World.translate; // Record input node coordinates
-				log::info("Input coords: {}", InputCoordinates);
-				if (InputCoordinates.Length() > 0 && DataCoordinates.Length() > 0) {
-					MovementForce = (InputCoordinates - DataCoordinates).Length();
-
-					// ^ Compare values, get movement force of Node X over 1 frame
-					// ^ And also compensate speed with scale, since nodes travel further distance at large distances
-				}
-
-				DataCoordinates = Node->World.translate; // Record new pos of bone
-				log::info("Data coords: {}", DataCoordinates);
-			}
+		float MovementForce = 0.0;
+		
+		switch (Type) {
+			case NodeMovementType::Movement_LeftLeg: 
+				Node = find_node(giant, "NPC L Foot [Lft ]");
+				DataCoordinates = Data.POS_Last_Leg_L;
+			break;
+			case NodeMovementType::Movement_RightLeg: 
+				Node = find_node(giant, "NPC R Foot [Rft ]");
+				DataCoordinates = Data.POS_Last_Leg_R;
+			break;
+			case NodeMovementType::Movement_LeftHand: 
+				Node = find_node(giant, "NPC L Hand [LHnd]");
+				DataCoordinates = Data.POS_Last_Hand_L;
+			break;
+			case NodeMovementType::Movement_RightHand: 
+				Node = find_node(giant, "NPC R Hand [RHnd]");
+				DataCoordinates = Data.POS_Last_Hand_R;
+			break;
 		}
+
+		if (Node) {
+			InputCoordinates = Node->world.translate; // Record input node coordinates
+			log::info("Input coords: {}", InputCoordinates);
+			if (InputCoordinates.Length() > 0 && DataCoordinates.Length() > 0) {
+				MovementForce = (InputCoordinates - DataCoordinates).Length();
+
+				// ^ Compare values, get movement force of Node X over 1 frame
+				// ^ And also compensate speed with scale, since nodes travel further distance at large distances
+			}
+
+			DataCoordinates = Node->world.translate; // Record new pos of bone
+			log::info("Data coords: {}", DataCoordinates);
+		}
+		
 		log::info("Movement Force: {}", MovementForce);
 		// Compensate speed with scale
 		if (MovementForce > 0) {
