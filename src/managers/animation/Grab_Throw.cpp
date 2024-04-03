9388dd7d839cb7a0f9c5f32b83061c3e21faf733
Sermitse <108320865+Sermitse@users.noreply.github.com>
2024-04-04 02:03:34 +0300

    Throw stuff again...

--- src/managers/animation/Grab_Throw.cpp
+++ src/managers/animation/Grab_Throw.cpp
@@ -78,12 +78,12 @@ namespace {
 
 				float distanceTravelled = vector.Length();
 				float timeTaken = endTime - startTime;
-				float speed = (distanceTravelled/timeTaken) * 10; // Standing throw default power
+				float speed = (distanceTravelled/timeTaken) * 12; // Standing throw default power
 
 				if (!giant->IsSneaking()) { // Goal is to fix standing throw direction
 
-					float angle_x = 10; // Runtime::GetFloat("cameraAlternateX"); // 10
-					float angle_y = 0; // Runtime::GetFloat("cameraAlternateY");//0.0;
+					float angle_x = Runtime::GetFloat("cameraAlternateX"); // 10
+					float angle_y = Runtime::GetFloat("cameraAlternateY");//0.0;
 					float angle_z = 0;//::GetFloat("combatCameraAlternateX"); // 0
 
 					// Conversion to radians
@@ -108,7 +108,7 @@ namespace {
 					direction = giantRot * (customDirection / customDirection.Length());
 				} else {
 				    if (IsCrawling(giant)) { // Strongest throw, needs custom throw direction again
-						speed *= 0.28; // Hand travels fast so it's a good idea to decrease its power
+						speed *= 0.24; // Hand travels fast so it's a good idea to decrease its power
 
 						float angle_x = 0;//Runtime::GetFloat("cameraAlternateX"); // 0
 						float angle_y = 0.008; // Runtime::GetFloat("cameraAlternateY");//0.008;
@@ -128,7 +128,7 @@ namespace {
 						direction = giantRot * (customDirection / customDirection.Length());
 					} else { // Else perform Slight Sneak Throw calc
 						direction = vector / vector.Length();
-						speed *= 0.10; // Hand also travels fast and we don't want this anim to feel strong
+						speed *= 0.08; // Hand also travels fast and we don't want this anim to feel strong
 					}
 				}
 
